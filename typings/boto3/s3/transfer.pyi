33354ca51dff82d765a42790cc12c6c3cd950859
BinaryBand <BinaryBand@users.noreply.github.com>
2026-04-06 20:27:19 +0200

    chore: remove local boto3/botocore/mypy_boto3_s3 typing stubs

--- typings/boto3/s3/transfer.pyi
+++
@@ -1,4 +0,0 @@
-from typing import Any
-
-class TransferConfig:
-    def __init__(self, *args: Any, **kwargs: Any) -> None: ...
--- typings/mypy_boto3_s3/type_defs.pyi
+++
@@ -1,3 +0,0 @@
-from typing import Any, Dict
-
-CopySourceTypeDef = Dict[str, Any]
